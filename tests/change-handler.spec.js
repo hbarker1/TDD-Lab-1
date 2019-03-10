a2dc56fc5c71d284ee3c1489afe83be6f3f8b44f
hannah barker <hannah.m.barker@gmail.com>
2019-03-10 14:10:21 -0400

    first commit

---
+++ tests/change-handler.spec.js
@@ -0,0 +1,14 @@
+// These test cases are all unfinished. We will finish them together.
+describe("tests for change-handler", function() {
+
+    // Set up a test below...
+    it("inserts coin and increases cash tendered", function() {
+        // Remember, you can arrange, act, and assert...start small
+        let changeHandler = new ChangeHandler({
+            amountDue: 20
+            
+        });
+        expect(changeHandler.insertCoin('penny')).toBe(1);
+    });
+
+});

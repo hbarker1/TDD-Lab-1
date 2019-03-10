7640c255c9624aec6a98b2643697478daa653fe7
hannah barker <hannah.m.barker@gmail.com>
2019-03-10 17:25:50 -0400

    adding parameters

--- src/change-handler.js
+++ src/change-handler.js
@@ -2,7 +2,16 @@
  * This class handles change for a vending machine.
  * 
  * IMPORTANT: All amounts are in cents. E.g. $1.35 = 135. This will help with rounding errors.
+
+
  */
+'use strict';
+
+module.exports = {
+    ChangeHandler
+  };
+
+
 class ChangeHandler {
     
     constructor(amountDue) {
@@ -14,23 +23,28 @@ class ChangeHandler {
      * The customer inserts a coin, increasing the cashTendered.
      * The parameter "type" is a string that is either quarter, dime, nickel, or penny
      */
-    insertCoin(type) {
-        return this.cashTendered + 1;
-    }
 
+
+    insertCoin(type) {
+        const type = ['quarter', 'dime', 'nickel', 'penny']
+        return this.cashTendered + 1;
+
+    }
     /**
      * Returns true if enough coins have been inserted to at least meet the amountDue
      */
     isPaymentSufficient() {
+        return this.amountDue = true;
+
     }
 
     giveChange() {
         // TODO return the correct change in the following format...
         return {
-            quarters: 0,
-            dimes: 0,
-            nickels: 0,
-            pennies: 0
+            quarters: 25,
+            dimes: 10,
+            nickels: 5,
+            pennies: 1
         }
     }
 }
--- tests/change-handler.spec.js
+++ tests/change-handler.spec.js
@@ -1,10 +1,13 @@
 // These test cases are all unfinished. We will finish them together.
+'use stricted';
+
 describe("tests for change-handler", function() {
 
     // Set up a test below...
     it("inserts coin and increases cash tendered", function() {
         // Remember, you can arrange, act, and assert...start small
         let changeHandler = new ChangeHandler({
+
             amountDue: 20
             
         });
